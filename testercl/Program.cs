23d5912ef404f0f89be87213993479de6a4db418
Peterson Andrade <tiago.peterson@gmail.com>
2017-07-17 20:52:25 -0300

    cli - removendo a classe criada

--- testercl/Program.cs
+++ testercl/Program.cs
@@ -14,6 +14,7 @@ namespace testercl
             Console.WriteLine("****Auto-Tester Console App****");
             Console.WriteLine("*******************************");
             Console.ReadKey();
+
         }
 
     }
